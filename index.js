716df83af5eb5411924388f31f33cd7cce780fa1
Nathan T Case <unclenate62@gmail.com>
2018-07-07 13:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeli) {
     for (let i = 0; i < katzDeli.length; i++) {
       katzDeliLineDisplay = katzDeliLineDisplay + (i + 1) + ". " + katzDeli[i];
       while i < katzDeli.length {
-        
+        katzDeliLineDisplay = katzDeliLineDisplay + ", ";
       }
     }
     return katzDeliLineDisplay
