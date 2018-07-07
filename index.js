c8871329043603265a2248f27523b44a80e13580
Nathan T Case <unclenate62@gmail.com>
2018-07-07 13:53:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ function currentLine(katzDeli) {
   var katzDeliLineDisplay = "The line is currently: ";
   if (katzDeli.length > 0) {
     for (let i = 0; i < katzDeli.length; i++) {
-      katzDeliLineDisplay = katzDeliLineDisplay + (i + 1) + ". " + katzDeli[i];
-      if (i < katzDeli.length) {
-        katzDeliLineDisplay = katzDeliLineDisplay + ", ";
-      }
+      katzDeliLineDisplay = katzDeliLineDisplay + (i + 1) + ". " + katzDeli[i] + ", "
   }
   return katzDeliLineDisplay
   }
